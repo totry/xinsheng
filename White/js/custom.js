a370c8c5902babaefc3d6fbce6251735b80247f2
kunsun <yangkunyjw@qq.com>
2014-11-23 11:09:35 +0800

    布局

--- White/js/custom.js
+++ White/js/custom.js
@@ -410,6 +410,21 @@ nextEffect : 'none',
 loop : false
 
 });
+$('.fancybox-thumbs').fancybox({
+        prevEffect : 'none',
+        nextEffect : 'none',
+
+        closeBtn  : false,
+        arrows    : false,
+        nextClick : true,
+
+        helpers : {
+          thumbs : {
+            width  : 50,
+            height : 50
+          }
+        }
+      });
 
 
 /* header fixed with sticky plugin */
